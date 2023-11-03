fdedd88a597920c752b16b3c3b977c8b4182bb0a
Greg Book <gregory.a.book@gmail.com>
2023-11-03 11:09:54 -0400

    make generic rpm build scripts

--- rpmbuild.sh
+++ rpmbuild.sh
@@ -6,6 +6,6 @@ rpmdev-setuptree
 wget https://github.com/gbook/squirrel/archive/main.zip
 unzip main.zip
 mv squirrel-main/* rpmbuild/SOURCES/
-cp -v rpmbuild/SOURCES/squirrel.el9.spec rpmbuild/SPECS/
+cp -v rpmbuild/SOURCES/squirrel.el.spec rpmbuild/SPECS/
 cd rpmbuild/SPECS
-QA_RPATHS=$((0x0002|0x0010)) rpmbuild -bb squirrel.el9.spec
+QA_RPATHS=$((0x0002|0x0010)) rpmbuild -bb squirrel.el.spec
