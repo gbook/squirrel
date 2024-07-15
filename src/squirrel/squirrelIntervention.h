f26ee145d5a9d591d8fd64fb9c1be90eabd6b20c
Greg Book <gregory.a.book@gmail.com>
2024-07-15 10:56:37 -0400

    formatting

--- src/squirrel/squirrelIntervention.h
+++ src/squirrel/squirrelIntervention.h
@@ -61,8 +61,8 @@ public:
     QString DoseKey;             /*!< for clinical trials, the dose key */
     QString DoseString;          /*!< full dose string (example "tylenol 325mg twice daily by mouth") */
     QString DoseUnit;            /*!< mg, g, ml, tablets, etc */
-    QString InterventionClass;           /*!< Intervention class */
-    QString InterventionName;            /*!< Intervention name (required) */
+    QString InterventionClass;   /*!< Intervention class (drug class) */
+    QString InterventionName;    /*!< Intervention name (required) */
     QString Notes;               /*!< freeform field for notes */
     QString Rater;               /*!< rater/experimenter/prescriber */
     double DoseAmount;           /*!< dose amount (required) */
